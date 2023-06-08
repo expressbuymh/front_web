be52e2b1f460761f8e5281258b6881240852ac77
Flavio Brito <flabrito43@gmail.com>
2023-06-08 17:21:53 -0300

    login fin

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import FormLogin from '../components/users/FormLogin'
 
-function SignIn () {
+export default function SignIn () {
   return (
-    <div>Signin</div>
+    <div className='flex justify-center items-center'>
+      <div className='sm:h-screen sm:w-full sm:flex sm:justify-center sm:items-center h-screen w-full flex justify-center items-center'>
+        <div className='sm:hidden h-screen w-1/2 bg-cover bg-center bg-no-repeat bg-bgsignup' />
+        <div className='sm:h-screen sm:w-full h-screen w-1/2 flex justify-center flex-col'>
+          <div className='flex items-center flex-col'>
+            <img src='' alt='' />
+            <h1 className='font-bold text-4xl m-3'>Welcome <span className='text-blue-800'>back</span>!</h1>
+          </div>
+          <FormLogin />
+        </div>
+      </div>
+    </div>
+
   )
 }
-
-export default SignIn
