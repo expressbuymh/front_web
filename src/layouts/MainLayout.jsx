6c031f0bc0439993535e960c1e6b4852e88bf32b
cartolanofacundo <cartolanofacundo@gmail.com>
2023-06-09 20:42:04 -0300

    main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,17 +1,25 @@
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
-export default function Main ({ children }) {
-/*   const location = useLocation()
-  if (['/Login', '/register'].includes(location.pathname)) {
-    return <Outlet />
-  } */
+export default function Main({ children }) {
+  /*   const location = useLocation()
+    if (['/Login', '/register'].includes(location.pathname)) {
+      return <Outlet />
+    } */
   return (
     <>
+      <div className='w-screen h-screen flex flex-col'>
+        <header>
+          <Navbar />
+        </header>
+        <main className='grow'>
+          {children}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
 
-      <Navbar />
-      {children}
-      <Footer />
 
     </>
   )
