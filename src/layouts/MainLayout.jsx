c325d59893b651f0fa1409b48e57c68af80cedcf
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-06-09 20:44:05 -0300

    Routes (#7)
    * router
    
    * main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,17 +2,19 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
 export default function Main ({ children }) {
-/*   const location = useLocation()
-  if (['/Login', '/register'].includes(location.pathname)) {
-    return <Outlet />
-  } */
   return (
     <>
-
-      <Navbar />
-      {children}
-      <Footer />
-
+      <div className='w-screen h-screen flex flex-col'>
+        <header>
+          <Navbar />
+        </header>
+        <main className='grow'>
+          {children}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </>
   )
 }
