89c30a7da36dbc40d7d9a83b969cbdc457cee0f2
Flavio Brito <flabrito43@gmail.com>
2023-06-15 21:59:37 -0300

    addresses terminada

--- src/pages/user/Adresses.jsx
+++ src/pages/user/Adresses.jsx
@@ -1,12 +1,64 @@
-import { Outlet } from "react-router-dom";
+import axios from "axios"
+import { api, endpoints, headers } from "../../utils/api"
+import { useNavigate, useParams } from "react-router"
+import { useEffect, useState } from "react"
+import { LS } from "../../utils/localStorageUtils"
+import { useSelector } from "react-redux"
+import { PlusCircleIcon } from "@heroicons/react/24/solid"
+import { Profile } from "./Profile"
+
 
 
 export function Addresses() {
-    return (
-        <>
-            <h1>Addresses</h1>
-            <Outlet/>
-        </>
+    
+    const [addresses,setAddresses]= useState([])
+    const navigate = useNavigate()
+   
 
+    useEffect(()=>{
+        api.get(endpoints.get_addresses,headers(LS.get("token")))
+        .then(res => {
+            setAddresses(res.data.addresses) 
+            
+          })
+        .catch(err => console.log(err))
+    }, [])
+
+    function editId(id,address) {
+        navigate(`/modify/${id}`,{state: address})
+      }
+
+  
+    return (
+        <div className="w-full h-full flex flex-col">
+            {addresses?.length > 0 ?
+            <div className="flex flex-col justify-center items-center mt-8">
+                <p className='text-paragraph-primary font-medium mt-2'>Your addresses</p>
+                <div className='w-11/12 flex flex-col justify-center items-center  border rounded-lg'>
+                    {addresses?.map((address) => {
+                        return <div className="h-32 w-10/12 grid grid-cols-4">
+                            <div className="flex flex-row gap-2 items-center justify-start">
+                                <p>Name:</p>
+                                {address?.name}
+                            </div>
+                            <div className="flex flex-row gap-4 items-center justify-center">
+                                <p>Direction:</p>
+                                <p>{address?.address_line}</p>  
+                            </div>
+                            <div className="flex flex-row gap-4 items-center justify-center"> 
+                                <p>State:</p>
+                                <p>{address?.state}</p>
+                            </div>
+                            <button onClick={()=>editId(address?._id,address)} className="text-primary-700 flex  items-center justify-start">Modify</button>
+                        </div>
+                    })}
+                </div>
+            </div>
+            :   <button /* onClick={handleAddAddress} */ className='w-full border flex flex-row gap-2 items-center justify-center bg-bg-medium text-paragraph-secondary p-2 rounded-lg my-2'>Create address 
+                    <PlusCircleIcon className='w-6 h-6 stroke-paragraph-secondary' />
+                </button>}
+                <button onClick={()=>navigate("/adresses/create")}>Create</button>
+            </div>
+            
     )
 }
--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -1,37 +1,51 @@
-import { HomeIcon, TruckIcon, ShoppingBagIcon } from "@heroicons/react/24/outline";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import { useSelector } from "react-redux";
-import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
+
 
 
 export function Profile() {
     const navigate = useNavigate()
-    const { user } = useSelector(store => store.user.data)
-    console.log(user)
+    const { user, addresses } = useSelector(store => store.user.data)
+    console.log(addresses?._id)
     return (
         <div className="w-full h-full flex lg:flex-row lg:justify-center lg:items-start md:flex-row flex-col justify-center items-center">
             <div className=" h-1/4 w-44 border mt-2 mx-4 rounded-full flex items-center justify-center">
-                <img className="object-contain rounded-full" src={user.photo} alt="" />
+                <img className="object-contain rounded-full" src={user?.photo} alt="" />
             </div>
             <div className="flex lg:w-3/5  w-11/12 flex-col gap-8">
                 <p className="font-bold mt-7">Profile</p>
                 <p className="text-sm font-light">This information will be available and it will be possible to make changes if necessary</p>
                 <div className="border-b mt-4 grid grid-cols-3">
                     <p className="w-1/4 font-medium">Nombre</p>
-                    <p className="w-1/4 ">{user.name}</p>
+                    <p className="w-1/4 ">{user?.name}</p>
                     <button className=" text-primary-700 font-black text-start">Change name</button>
                 </div>
                 <div className="border-b mt-4 grid grid-cols-3">
                     <p className="w-1/4 font-medium">Last name</p>
-                    <p className="w-1/4 ">{user.last_name}</p>
+                    <p className="w-1/4 ">{user?.last_name}</p>
                     <button className=" text-primary-700 font-black text-start">Change Last name</button>
                 </div>
                 <div className="border-b mt-4 grid grid-cols-3">
                     <p className="w-1/4 font-medium">Email</p>
-                    <p className="">{user.email}</p>
+                    <p className="">{user?.email}</p>
                 </div>
-                <div className="mt-4">
-                    <button onClick={() => navigate("/")} className="w-1/3 mr-2 bg-primary-700 rounded-lg h-8 font-semibold text-white">Home</button>
-                    <button onClick={() => navigate("/user/orders")} className="w-1/3 mx-2 bg-primary-700 rounded-lg h-8 font-semibold text-white">My orders</button>
+                {addresses?.length > 0 ?
+                    <div className="grid grid-cols-3 items-center">
+                        <p className='text-paragraph-primary font-medium mt-2'>Shipment address</p>
+                        <select className='w-full border rounded-lg p-2 mt-2'>
+                            {addresses?.map((address) => {
+                                return <option>
+                                    {address?.name}
+                                </option>
+                            })}
+                        </select>
+                    </div>
+                    : <button /* onClick={handleAddAddress} */ className='w-full border flex flex-row gap-2 items-center justify-center bg-bg-medium text-paragraph-secondary p-2 rounded-lg my-2'>Create address <PlusCircleIcon className='w-6 h-6 stroke-paragraph-secondary' /></button>}
+                <div className="mt-4 grid grid-cols-3">
+                    <button onClick={() => navigate("/")} className=" mr-2 bg-primary-700 rounded-lg h-8 font-semibold text-white">Home</button>
+                    <button onClick={() => navigate("/user/orders")} className="mx-2 bg-primary-700 rounded-lg h-8 font-semibold text-white">My orders</button>
+                    <button onClick={() => navigate("/adresses/me")} className="mr-2 bg-primary-700 rounded-lg h-8 font-semibold text-white">My addresses</button>
                 </div>
             </div>
 
